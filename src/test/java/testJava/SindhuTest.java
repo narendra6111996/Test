0a55d60e6fa5745c8d688bb6ca2b2211c93d8491
sindhu <sindhujapuppala7@gmail.com>
2023-12-27 16:25:24 +0530

    sindhuTest modified

--- src/test/java/testJava/SindhuTest.java
+++ src/test/java/testJava/SindhuTest.java
@@ -4,6 +4,7 @@ public class SindhuTest {
 	 
 	public void test() {
 		System.out.println("sindhu test added");
+		System.out.println("testing file");
 	}
 
 }
---
+++ src/test/java/testJava/TestDemo.java
@@ -0,0 +1,9 @@
+package testJava;
+
+public class TestDemo {
+	
+	public TestDemo() {
+		System.out.println("Test Demo");
+	}
+
+}
