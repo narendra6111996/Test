75b5d7acb59dfedde099e9a680216bff2d9365df
sindhu <sindhujapuppala7@gmail.com>
2023-12-27 11:26:59 +0530

    demo

--- src/test/java/testJava/DemoOne.java
+++ src/test/java/testJava/DemoOne.java
@@ -4,6 +4,9 @@ public class DemoOne {
 	
 	public static void main(String[] args) {
 		System.out.println("demo one");
+		System.out.println("demo two");
+		System.out.println("demo three");
+	
 	}
 
 }
