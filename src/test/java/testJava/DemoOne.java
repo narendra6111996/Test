2f77db74605734c72f5fef6c91e981636c9d42a6
sindhu <sindhujapuppala7@gmail.com>
2023-12-27 11:40:13 +0530

    new statement added

--- src/test/java/testJava/DemoOne.java
+++ src/test/java/testJava/DemoOne.java
@@ -1,13 +1,13 @@
 package testJava;
 
 public class DemoOne {
-	
+
 	public static void main(String[] args) {
 		System.out.println("demo one");
 		System.out.println("demo two");
 		System.out.println("demo three");
 		System.out.println("This statement is added by Narendra");
-
+		System.out.println("This statement is added by Sindhu");
 	}
 
 }
